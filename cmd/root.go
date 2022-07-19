5ad24a000a7208f9d343b161cd5b007994664481
Michael Messmore <mike@messmore.org>
2022-07-19 16:57:38 -0500

    Stop saying it's using the config file

--- cmd/root.go
+++ cmd/root.go
@@ -21,7 +21,6 @@ THE SOFTWARE.
 package cmd
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -88,7 +87,5 @@ func initConfig() {
 	viper.AutomaticEnv() // read in environment variables that match
 
 	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
-	}
+	viper.ReadInConfig()
 }
